41d4a1babf6e5eb8571fc7bda8550915214b5ba8
Frizzybman11 <Frizzybman11@aol.com>
2024-01-03 17:39:42 -0500

    Align game elements to center

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const letterButton = document.querySelectorAll(".letterButton")
 
 const wordLibrary = ['amber', 'aromas', 'autumn', 'blood', 'bonfire', 'bounty', 'brisk', 'broomstick', 'candle', 'candy', 'carving', 'chestnuts', 'chili', 'chilly', 'cider', 'cinnamon', 'cornstalk', 'cornucopia', 'costume', 'cranberry', 'creepy', 'crisp', 'crunching', 'disguise', 'equinox', 'family', 'fangs', 'feast', 'festival', 'foliage', 'frosty', 'gathering', 'generous', 'ghost', 'ghoulish', 'goblin', 'golden', 'gourds', 'grandparents', 'graveyard', 'gravy', 'halloween', 'harvest', 'haunted', 'hayride', 'holiday', 'inviting', 'kernel', 'maize', 'monster', 'mummy', 'november', 'october', 'orchard', 'parade', 'party', 'pirate', 'plenty', 'prince', 'princess', 'pumpkin', 'raking', 'roasting', 'rustling', 'savory', 'scarecrow', 'scary', 'season', 'september', 'skeleton', 'skull', 'slimy', 'spicy', 'spooky', 'sweater', 'thankful', 'thanksgiving', 'trail', 'turkey', 'vampire', 'vibrant', 'werewolf', 'wicked', 'witch', 'zombie']
 
-const textArea = document.querySelector('#textArea')
+const gameArea = document.querySelector('#gameArea')
 
 const gameOver = document.createElement('span');
 gameOver.id = "gameOver"
@@ -36,13 +36,13 @@ letterButton.forEach((button) => {
             if (wordArray[0] == wordArray[1]){
                 gameStatus = "Complete"
                 gameOver.textContent = "You win!"
-                textArea.appendChild(gameOver)
+                gameArea.appendChild(gameOver)
                 resetArea.appendChild(reset)
             }
             if (strikeCount == 10){
                 gameStatus = "Complete"
                 gameOver.textContent = "You lose!"
-                textArea.appendChild(gameOver)
+                gameArea.appendChild(gameOver)
                 resetArea.appendChild(reset)
             } 
         }
@@ -56,7 +56,7 @@ reset.addEventListener('click', () => {
     gameWord.textContent = wordArray[0]
     gameStrikes.textContent = "Strikes: 0 / 10"
     let hiddenWord = ""
-    textArea.removeChild(gameOver)
+    gameArea.removeChild(gameOver)
     resetArea.removeChild(reset)
     for (button of letterButton){
         button.disabled = false;
